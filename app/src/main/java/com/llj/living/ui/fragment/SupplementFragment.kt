719e13b763fcbbcac4ef3bac9c23d3c18c878413
LLJ <1175637511@qq.com>
2021-03-04 23:52:13 +0800

    1、新增底部导航+补录viewPager2二级联动 2、优化部分UI tableLayout字号 颜色 涟漪效果 bottomNavigation 点击效果

---
+++ app/src/main/java/com/llj/living/ui/fragment/SupplementFragment.kt
@@ -0,0 +1,40 @@
+package com.llj.living.ui.fragment
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.llj.living.R
+
+class SupplementFragment : BaseFragment() {
+
+    override fun getLayoutId(): Int = R.layout.fragment_supplement
+
+    lateinit var viewPager2: ViewPager2
+    lateinit var tabLayout: TabLayout
+
+    override fun init() {
+        viewPager2 = requireView().findViewById(R.id.viewpager2_supple_doing)
+        tabLayout = requireView().findViewById(R.id.tab_layout_supple_doing)
+
+        viewPager2.adapter = object : FragmentStateAdapter(requireActivity()) {
+            override fun getItemCount(): Int = 2
+
+            override fun createFragment(position: Int): Fragment {
+                return if (position == 0) {
+                    SuppleDoingFragment()
+                } else {
+                    SuppleFinishFragment()
+                }
+            }
+        }
+
+        TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+
+            if (position == 0) tab.text = resources.getString(R.string.doing)
+            else tab.text = resources.getString(R.string.finished)
+        }.attach() //沙雕 不加attach()是不会显示顶部文字的
+
+    }
+}
