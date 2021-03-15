bbaa8b93cc3a109c1a686b7ae72c806f369c35e4
LLJ <1175637511@qq.com>
2021-03-15 21:43:07 +0800

    1、补录详情viewModel封装 修改dataBinding

--- app/src/main/java/com/llj/living/ui/activity/ActivitySuppleInfo.kt
+++ app/src/main/java/com/llj/living/ui/activity/ActivitySuppleInfo.kt
@@ -1,31 +1,51 @@
 package com.llj.living.ui.activity
 
 import android.view.View
+import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.llj.living.R
 import com.llj.living.data.bean.ToolbarConfig
+import com.llj.living.data.enums.IsShowedType
 import com.llj.living.data.enums.TakePhotoEnum
 import com.llj.living.databinding.ActivitySupplementInfoBinding
+import com.llj.living.logic.vm.SuppleInfoVM
 import kotlinx.coroutines.launch
 
-class ActivitySuppleInfo:BaseTPActivity<ActivitySupplementInfoBinding>() {
+class ActivitySuppleInfo : BaseTPActivity<ActivitySupplementInfoBinding>() {
 
     override fun getLayoutId(): Int = R.layout.activity_supplement_info
 
     private val isAllCompleted = mutableSetOf<Int>()
 
+    private lateinit var viewModel: SuppleInfoVM
+
     override fun init() {
-        setToolbar(ToolbarConfig("补录信息",isShowBack = true,isShowMenu = false))
+        setToolbar(ToolbarConfig("补录信息", isShowBack = true, isShowMenu = false))
+        viewModel = initViewModel<SuppleInfoVM>()
         getDataBinding().apply {
+            lifecycleOwner = this@ActivitySuppleInfo
+            asiVM = viewModel
             tvTakePhotoFace.setOnClickListener {
                 takePhoto(TakePhotoEnum.PersonFace)
             }
+            reFace.setOnClickListener {
+                takePhoto(TakePhotoEnum.PersonFace)
+            }
+
             tvTakePhotoIdA.setOnClickListener {
                 takePhoto(TakePhotoEnum.IdcardFront)
             }
+            reIdCardA.setOnClickListener {
+                takePhoto(TakePhotoEnum.IdcardFront)
+            }
+
             tvTakePhotoIdB.setOnClickListener {
                 takePhoto(TakePhotoEnum.IdcardBehind)
             }
+            reIdCardB.setOnClickListener {
+                takePhoto(TakePhotoEnum.IdcardBehind)
+            }
+
             btFinishSuppleInfo.setOnClickListener {
                 finish()
             }
@@ -34,50 +54,53 @@ class ActivitySuppleInfo:BaseTPActivity<ActivitySupplementInfoBinding>() {
 
     private fun takePhoto(personFace: TakePhotoEnum) {
         lifecycleScope.launch {
-            when(personFace){
-                TakePhotoEnum.PersonFace->faceLaunch.launch(getPhotoIntent(personFace))
-                TakePhotoEnum.IdcardFront->frontLaunch.launch(getPhotoIntent(personFace))
-                TakePhotoEnum.IdcardBehind->behindLaunch.launch(getPhotoIntent(personFace))
+            when (personFace) {
+                TakePhotoEnum.PersonFace -> faceLaunch.launch(getPhotoIntent(personFace))
+                TakePhotoEnum.IdcardFront -> frontLaunch.launch(getPhotoIntent(personFace))
+                TakePhotoEnum.IdcardBehind -> behindLaunch.launch(getPhotoIntent(personFace))
             }
         }
     }
 
     private val faceLaunch by lazy {
-        buildLaunch {
-            if (it==null) return@buildLaunch
-            getDataBinding().apply {
-                ivFaceSuppleInfo.setImageBitmap(it)
-                tvTakePhotoFace.visibility = View.INVISIBLE
+        buildLaunch { bitmap ->
+            bitmap.let {
+                getDataBinding().apply {
+                    ivFaceSuppleInfo.setImageBitmap(it)
+                    viewModel.setShowedUi(IsShowedType.Face)
+                }
+                checkIsCompleted(1)
             }
-            checkIsCompleted(1)
         }
     }
 
     private val frontLaunch by lazy {
-        buildLaunch {
-            if (it==null) return@buildLaunch
-            getDataBinding().apply {
-                ivIdCardASuppleInfo.setImageBitmap(it)
-                tvTakePhotoIdA.visibility = View.INVISIBLE
+        buildLaunch { bitmap ->
+            bitmap?.let {
+                getDataBinding().apply {
+                    ivIdCardASuppleInfo.setImageBitmap(it)
+                    viewModel.setShowedUi(IsShowedType.IdCardA)
+                }
+                checkIsCompleted(2)
             }
-            checkIsCompleted(2)
         }
     }
 
     private val behindLaunch by lazy {
-        buildLaunch {
-            if (it==null) return@buildLaunch
-            getDataBinding().apply {
-                ivIdCardBSuppleInfo.setImageBitmap(it)
-                tvTakePhotoIdB.visibility = View.INVISIBLE
+        buildLaunch { bitmap ->
+            bitmap?.let {
+                getDataBinding().apply {
+                    ivIdCardBSuppleInfo.setImageBitmap(it)
+                    viewModel.setShowedUi(IsShowedType.IdCardB)
+                }
+                checkIsCompleted(3)
             }
-            checkIsCompleted(3)
         }
     }
 
-    private fun checkIsCompleted(tag:Int){
+    private fun checkIsCompleted(tag: Int) {
         isAllCompleted.add(tag)
-        if (isAllCompleted.size >= 3){
+        if (isAllCompleted.size >= 3) {
             getDataBinding().btFinishSuppleInfo.visibility = View.VISIBLE
         }
     }
