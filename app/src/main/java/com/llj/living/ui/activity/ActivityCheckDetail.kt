1585dd31c69a30a1da1efcab97ec2401cccf9e32
LLJ <1175637511@qq.com>
2021-03-30 01:03:31 +0800

    1、room实现数据本地持久化 2、补录 审核数据联通 3、比赛作品版完成

--- app/src/main/java/com/llj/living/ui/activity/ActivityCheckDetail.kt
+++ app/src/main/java/com/llj/living/ui/activity/ActivityCheckDetail.kt
@@ -1,36 +1,95 @@
 package com.llj.living.ui.activity
 
 import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import com.llj.living.R
+import com.llj.living.custom.ext.toSimpleTime
 import com.llj.living.data.bean.ToolbarConfig
+import com.llj.living.data.database.OldManInfoWait
+import com.llj.living.data.enums.TakePhotoEnum
 import com.llj.living.databinding.ActivityCheckDetailBinding
+import com.llj.living.logic.vm.DatabaseVM
+import com.llj.living.ui.adapter.CheckDoingAdapter
+import com.llj.living.ui.adapter.WaitCheckAdapter
+import com.llj.living.utils.ToastUtils
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class ActivityCheckDetail:BaseActivity<ActivityCheckDetailBinding>() {
+class ActivityCheckDetail : BaseTPActivity<ActivityCheckDetailBinding>() {
 
     override fun getLayoutId(): Int = R.layout.activity_check_detail
 
+    //    private val TAG = this.javaClass.simpleName
+    private val dbViewModel by viewModels<DatabaseVM>()
+    private var bean: OldManInfoWait ?= null
+
     override fun init() {
-        setToolbar(ToolbarConfig(getString(R.string.check_detail),isShowBack = true,isShowMenu = true))
+        setToolbar(
+            ToolbarConfig(
+                getString(R.string.check_detail),
+                isShowBack = true,
+                isShowMenu = false
+            )
+        )
+
         getDataBinding().apply {
-            ivOldManPhoto.setOnClickListener {
-                getFacePhoto()
+            takePhotoFragment.setOnClickListener {
+                lifecycleScope.launch {
+                    launchOldManPhoto.launch(getPhotoIntent(TakePhotoEnum.PersonFace))
+                }
             }
+            btCompleted.setOnClickListener {
+                lifecycleScope.launch(Dispatchers.IO) {
+                    updateData()
+                    delay(500)
+                    finish()
+                }
+            }
+        }
+
+        val id = WaitCheckAdapter.id
+        dbViewModel.getOldManInfoById(id).observe(this, Observer {data->
+            data?.let {
+                bean = it
+            }
+        })
+
+    }
+
+    private fun updateData() {
+        bean?.let {
+            dbViewModel.finishedOneHad(it,CheckDoingAdapter.id)
         }
     }
 
     private fun getFacePhoto() {
         lifecycleScope.launch {
             delay(500)
-            getDataBinding().apply{
-                btCompleted.visibility = View.VISIBLE
-                tvTipsTakePhoto.visibility = View.INVISIBLE
-                ivOldManPhoto.setImageResource(R.mipmap.logo)
+            bean?.let {
+                getDataBinding().apply {
+                    btCompleted.visibility = View.VISIBLE
+                    tvTipsTakePhoto.visibility = View.INVISIBLE
+                    tvTips.visibility = View.INVISIBLE
+                    tvSex.text = it.sex
+                    tvFullName.text = it.name
+                    tvCheckIdCard.text = it.idCard
+                    tvCheckTime.text = System.currentTimeMillis().toSimpleTime()
+                }
+                ToastUtils.toastShort("识别成功")
             }
         }
-
     }
 
+    /**
+     * 构建照相启动模式
+     */
+    private val launchOldManPhoto by lazy {
+        buildLaunch {
+            getDataBinding().ivOldManPhoto.setImageBitmap(it)
+            getFacePhoto()
+        }
+    }
 }
