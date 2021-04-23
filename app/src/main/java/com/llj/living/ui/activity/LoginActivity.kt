dbe43a06040f34faec4c9434f8b1d932c7433ac3
LLJ <1175637511@qq.com>
2021-04-23 22:14:20 +0800

    1、activity viewModel扩展函数封装 2、业务网络请求基类抽取

--- app/src/main/java/com/llj/living/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/llj/living/ui/activity/LoginActivity.kt
@@ -1,55 +1,47 @@
 package com.llj.living.ui.activity
 
-import androidx.lifecycle.lifecycleScope
+import androidx.activity.viewModels
 import com.llj.living.R
+import com.llj.living.application.MyApplication
 import com.llj.living.custom.ext.baseObserver
+import com.llj.living.custom.ext.startCommonFinishedActivity
+import com.llj.living.custom.ext.toastShort
 import com.llj.living.data.bean.ToolbarConfig
 import com.llj.living.databinding.ActivityLoginBinding
 import com.llj.living.logic.vm.LoginViewModel
-import kotlinx.coroutines.launch
 
-class LoginActivity : BaseActivity<ActivityLoginBinding>() {
+class LoginActivity : BaseBLActivity<ActivityLoginBinding>() {
 
     override fun getLayoutId(): Int = R.layout.activity_login
 
-    private lateinit var viewModel: LoginViewModel
+    private val viewModel by viewModels<LoginViewModel>()
 
     override fun init() {
-        setToolbar(ToolbarConfig(title = "LoginActivity", isShowBack = false, isShowMenu = false))
-        initListener()
+        setToolbar(ToolbarConfig(title = "用户登录", isShowBack = false, isShowMenu = false))
         initVM()
+        initLD()
+    }
+
+    private fun initLD() {
+        viewModel.loginResultLiveData.baseObserver(this) { isSuc ->
+            if (isSuc) {
+                startCommonFinishedActivity<MainActivity>()
+            }
+        }
+        viewModel.toastMsgLiveData.baseObserver(this) {
+            if (it.isNotEmpty()) toastShort(it)
+        }
+        locationLiveData.baseObserver(this) {
+            viewModel.setLocation(it)
+        }
     }
 
     private fun initVM() {
-        viewModel = initViewModel<LoginViewModel>()
-
-        viewModel.getRememberPwdLiveData().baseObserver(this) {
-            getDataBinding().cbRemembPwdLogin.isChecked = it
-        }
-
         getDataBinding().loginVm = viewModel
-
+        val versionStr =
+            "${resources.getString(R.string.user_version)}${MyApplication.CURRENT_VERSION}"
+        getDataBinding().tvVersionLogin.text = versionStr
         viewModel.loadUserData()
-    }
-
-    private fun initListener() {
-        getDataBinding().btLoginActivity.setOnClickListener {
-            viewModel.getPassWordLiveData().postValue((0..5).random().toString())
-        }
-        getDataBinding().cbRemembPwdLogin.apply {
-            setOnClickListener {
-                viewModel.getRememberPwdLiveData().postValue(this.isChecked)
-            }
-        }
-        getDataBinding().btLoginActivity.setOnClickListener {
-            lifecycleScope.launch {
-                val isSuc = viewModel.login()
-                if (isSuc) {
-                    startCommonActivity<MainActivity>()
-                    finish()
-                }
-            }
-        }
     }
 
 }
